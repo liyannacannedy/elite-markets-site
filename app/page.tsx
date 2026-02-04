c6246dad4a75ac61a2ee00549310742d59a050ed
Liyanna Cannedy <elite_markets@myyahoo.com>
2026-02-03 19:13:53 -0800

    Fix homepage build error

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,3 @@ import EliteMarketsSite from "./EliteMarketsSite";
 export default function Page() {
   return <EliteMarketsSite />;
 }
-import EliteMarketsSite from "./EliteMarketsSite";
-
-export default function Page() {
-  return <EliteMarketsSite />;
-}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
-}
